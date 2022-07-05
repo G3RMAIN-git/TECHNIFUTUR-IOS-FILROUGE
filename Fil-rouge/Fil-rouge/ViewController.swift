2202533efb44968b2dcafda7f8a72009a6f27d65
Germain Buchet <germain.buch@gmail.com>
2022-07-05 13:39:38 +0200

    Initialisation écran login + appicone

--- Fil-rouge/Fil-rouge/ViewController.swift
+++ Fil-rouge/Fil-rouge/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var login: UITextField!
+    @IBOutlet var password: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        login.delegate = self
+        password.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
+    extension ViewController: UITextFieldDelegate {
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            view.endEditing(true)
+        }
 
 }
 
